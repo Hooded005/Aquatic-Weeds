47ac37e085ec8c39864656ee5c982df6003ac2aa
ALOnelyAVacado <Stefan.sutherland1@outlook.com>
2024-11-01 18:09:13 +0200

    Added new html file and made the "Button" button to "Get Known Coverage"

---
+++ src/website/myapp/static/test.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+  console.log("Test-Load");
+});
+
+const temp =document.getElementById('hyperlink-button');
+//temp.addEventListener("input", test());
+
+function test(){
+    console.log("Function Called")
+};
+
