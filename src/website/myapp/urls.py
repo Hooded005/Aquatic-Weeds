875edb5b547bb72454028e239725d770d0db4984
ALOnelyAVacado <Stefan.sutherland1@outlook.com>
2024-11-19 21:35:22 +0200

    Page now keeps grid values when cycling between images.

--- src/website/myapp/urls.py
+++ src/website/myapp/urls.py
@@ -1,8 +1,8 @@
-# website/urls.py
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.prediction_view, name='prediction_view'),
+    path('prediction_view/', views.prediction_view, name='prediction_view'),
     path('cycle/<str:direction>/', views.cycle_data_view, name='cycle_data'),
 ]
--- src/website/myapp/views.py
+++ src/website/myapp/views.py
@@ -19,7 +19,17 @@ def prediction_view(request):
     global my_predictions, avg_size
     data = []  # Default empty data in case the user hasn't submitted the form
     alert_message = None  # Initialize alert message
-    if request.method == 'POST':
+
+    if request.GET.get('keep_data') == 'true':
+        data = [
+            {'Date': pred[0],
+             'Temperature': pred[1],
+             'Wind_Speed': pred[2],
+             'Wind_Direction': pred[3],
+             'Start_Size': str(pred[4]) + '%',
+             'Predicted_End_Size': str(pred[5]) + '%'}
+            for pred in my_predictions]
+    elif request.method == 'POST':
         start_size = float(request.POST.get('start_size_input', 0))
         days = int(request.POST.get('days_input', 0))
 
@@ -64,7 +74,7 @@ def cycle_data_view(request, direction):
     elif direction == 'previous':
         array_pos = (array_pos - 1 + len(my_predictions)) % len(my_predictions)
 
-    return redirect('prediction_view')
+    return redirect(f'/prediction_view?keep_data=true')
 
 
 def redo_predictions(start_size=5, days=5):
